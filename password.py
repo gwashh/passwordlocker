c44f98a6f5452f73f1aa8311e6799bad8f7662ff
gwashh <perrysugut75@gmail.com>
2022-04-26 11:04:25 +0300

    first commit

---
+++ password.py
@@ -0,0 +1,101 @@
+import pyperclip
+
+
+class User:
+    """
+    Create user class to add new user
+    """
+    user_list = []
+
+def __init__(self, username, password):
+    """
+    method that allows us to define properties of a user
+    """
+    self.username = username
+    self.password = password
+
+def save_user(self): 
+    """  
+    method that saves a new user
+    """
+    User.user_list.append(self)
+
+    @classmethod 
+    def show_user(cls):
+      return cls.user_list
+
+class Credentials():
+   """
+   Creates credential class so as to create new object
+   """ 
+   credentials_list = []
+
+   def _init_(self,account,userName,password):
+        """ 
+        method that tells user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password 
+
+@classmethod
+def verify_user(cls,username,password):   
+    """
+    method to confirm whether the user is in our user_list
+    """
+    for details in cls.credential_list:
+      if details.username ==username:
+          return details  
+
+def save_details(self):
+        """
+        method to save a new credential to the credentials list
+        """   
+        Credentials.credentials_list.append(self)
+
+def delete_credentials(self):
+    """
+    method used to delete credentials from the credentials list
+    """
+    Credentials.credentials_list.remove(self)
+
+@classmethod
+def find_credentials(cls,account):
+    """
+    method that collects an account_name and outputs the credential that matches the account_name
+    """
+    for credentials in cls.credentials_list:
+        if credentials.account == account:
+            return credentials
+
+@classmethod
+def copy_password  (cls,account):
+    found_credentials =Credentials.find_credential(account)
+    pyperclip.copy(found_credentials.password)
+   
+@classmethod
+def if_credential_exist(cls,account):
+   """
+   method to check if credential is in credential list then returns true if credential exist and false if credential does not exist
+   """
+   for credential in cls.credentials_list:
+       if credential.account == account:
+           return True
+       return False
+
+@classmethod 
+
+def display_credentials(cls):
+    """
+    method that returns all items in credential list
+    """    
+    return cls.credentials_list
+
+def generate_password():
+    """
+    generate random password
+    """
+    
+
+
+
---
+++ password_test.py
@@ -0,0 +1,106 @@
+import unittest
+from password import User
+from password import Credentials
+
+class TestUser(unittest.TestCase):
+    """
+    a test class that defines test cases for the User class
+    """
+
+def setUp(self):
+    """
+    method that is executed before each individual tets methods run
+    """
+    self.new_user = User('Perry','Slime')
+
+def test_init(self):
+    """
+    a test to confirm whether the object is working correctly
+    """
+    self.assertEqual(self.new_user.username,'perry')
+    self.assertEqual(self.new_user.password,'slime')
+
+def test_save_user(self):
+    """
+    a test to check if new user has been added to User list
+    """
+    self.new_user.save_user()
+    self.assertEqual(User.user_list)
+
+class TestCredentials(unittest.TestCase):
+    """
+    a test that defines test casee for the credentials class
+    """    
+
+def setUp(self):
+    """
+    method that is executed before each individual credentialtest method runs
+    """
+    self.new_credential= Credentials('Gmail', 'perry', 'slime')
+
+def test_init(self):
+    """
+    test to check whether a new credential information has been entered correctly
+    """
+    self.assertEqual(self.new_credential.account,'Gmail')
+    self.assertEqual(self.new_credential.username,'Perry')
+    self.assertEqual(self.new_credential.passwword,'slime')
+
+def save_credential_test(self):
+    """
+    test to check whether the credential is saved to the credential list
+    """
+    self.new_credential.save_detail()
+    self.assertEqual(Credentials.credentials_list)
+
+Credentials.credentils_list = []
+
+def test_save_many_accounts(self):
+    """
+    test to see if we can save many accounts
+    """
+    self.new_credentials.save_detail()
+    test_credentials = Credentials('Gmail', 'Kentrell','deshaun')
+    test_credentials.save_details()
+    self.assertEqual(Credentials.credentils_list)
+
+def test_delete_credential(self):
+    """
+    test to see if we can delete an account from our credentials list
+    """
+    self.new_credential.save_details()
+    test_credential = Credentials('Gmail', 'Kentrell', 'deshaun')
+    test_credential.save_details()
+
+    self.new_credential.delete_credential()
+    self.assertEqual(Credentials.credentils_list)
+
+def test_find_credential(self):
+    """
+    test to see if we can find a credential by using the account name and show the details
+    """
+    self.new_credential.save_details()
+    test_credential = Credentials('Gmail', 'Kenterll', 'deshaun')
+    test_credential.save_details
+
+    the_credential = Credentials.find_credential('Gmail')
+    self.assertEqual(the_credential.account,test_credential.account)
+
+def test_credential_exist(self):
+    """
+    test to if we can output yes or no if the credential exists
+    """    
+    self.new_credential.save_details()
+    the_credential = Credentials('Gmail', 'Kentrell', 'deshaun')
+    the_credential.save_details()
+    credential_is_found = Credentials.if_credential_exist('Gmail')
+    self.assertEqual(credential_is_found)
+
+def test_display_all_saved_credentials(self):
+    """
+    test that shows all saved credentials in the credential list
+    """    
+    self.assertEqual(Credentials.diplay_credentials(),Credentials.credentils_list)
+
+if __name__ == "__main__":
+    unittest.main()
